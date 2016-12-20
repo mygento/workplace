f1f0e94e657ba7a16684c94413f259eddbc6405c
Nikita Tarasov <nikita@mygento.ru>
2016-12-20 14:21:22 +0300

    fix owner

--- start.sh
+++ start.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env sh
 sudo groupadd -g 33 www-data
 sudo useradd --uid 33 --gid 33 -d /var/www/ -s /bin/sh www-data
+sudo usermod -a -G www-data ${USER}
 composer install
 npm install
 docker-compose build
 docker-compose up -d
-chown -R www-data:www-data application
+chgrp -R www-data application
 gulp

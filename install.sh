502873b12ad5520146bc33d263dd0d736e9d97c5
Nikita Tarasov <nikita@mygento.ru>
2016-12-20 14:42:49 +0300

    split install and start + update gitignore

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+sudo groupadd -g 33 www-data
+sudo useradd --uid 33 --gid 33 -d /var/www/ -s /bin/sh www-data
+sudo usermod -a -G www-data ${USER}
+composer install
+npm install
+docker-compose build
+docker-compose pull
--- start.sh
+++ start.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env sh
-sudo groupadd -g 33 www-data
-sudo useradd --uid 33 --gid 33 -d /var/www/ -s /bin/sh www-data
-sudo usermod -a -G www-data ${USER}
-composer install
-npm install
-docker-compose build
 docker-compose up -d
 chgrp -R www-data application
+chmod a+w application/public/app/etc
+chmod -R a+w application/public/media
 gulp

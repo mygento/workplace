62e34a37e4d92d2a39dd5165a048a1c74022c946
Nikita <luckyraul@users.noreply.github.com>
2016-12-20 11:49:44 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env sh
+sudo groupadd -g 33 www-data
+sudo useradd --uid 33 --gid 33 -d /var/www/ -s /bin/sh www-data
 composer install
 npm install
 docker-compose build
 docker-compose up -d
+chown -R www-data:www-data application
 gulp

ad568a9ffda9aad776641d2ab736aa44ff7a873f
Nikita <luckyraul@users.noreply.github.com>
2016-12-29 17:40:55 +0300

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env sh
-sudo apt-get update && sudo apt-get install apt-transport-https ca-certificates gnupg2
-sudo apt-key adv \
-       --keyserver hkp://ha.pool.sks-keyservers.net:80 \
-       --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+sudo apt-get update && sudo apt-get install -y apt-transport-https ca-certificates gnupg2
+sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 sudo echo 'deb https://apt.dockerproject.org/repo debian-jessie main' > /etc/apt/sources.list.d/docker.list
-sudo apt-get update && sudo apt-get install docker-engine
+sudo apt-get update && sudo apt-get install -y docker-engine
 sudo groupadd docker
 sudo gpasswd -a ${USER} docker
 sudo service docker restart

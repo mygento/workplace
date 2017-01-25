d6b3230508cc60716e3a8eacd7bc43bcac31acc9
Nikita <luckyraul@users.noreply.github.com>
2017-01-25 18:49:15 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 sudo groupadd -g 33 www-data
 sudo useradd --uid 33 --gid 33 -d /var/www/ -s /bin/sh www-data
 sudo usermod -a -G www-data ${USER}
+rm -f application/composer.lock
 composer install -d application --ignore-platform-reqs
 npm install
 docker-compose pull

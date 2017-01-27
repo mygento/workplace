110bdc726500a4f21413ea5f2e199b35a85349a6
Nikita <luckyraul@users.noreply.github.com>
2017-01-27 12:33:20 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,6 +2,7 @@
 docker-compose up -d
 docker exec -ti m2-php chmod +x /var/www/public/bin/magento
 chgrp -R www-data application
+chmod g+s application
 chmod -R a+w application/app/etc
 chmod -R a+w application/var
 chmod -R a+w application/pub/media

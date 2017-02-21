dd2c740513a080cfc563291a730831e72797acb1
Nikita <luckyraul@users.noreply.github.com>
2017-02-21 15:56:06 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 docker-compose up -d
-chgrp -R www-data application
-chmod a+w application/public/app/etc
-chmod -R a+w application/public/media
+chmod g+s application
+chmod -R g+w application/public/media
+chmod -R g+w application/public/app/etc
+chmod -R g+w application/public/var
 gulp

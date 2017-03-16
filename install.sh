2542feb4cd47e8642c4f9a6f927363ba7d5644b8
Nikita <luckyraul@users.noreply.github.com>
2017-03-16 18:38:00 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env sh
-sudo groupadd -g 33 www-data
-sudo useradd --uid 33 --gid 33 -d /var/www/ -s /bin/sh www-data
 sudo usermod -a -G www-data ${USER}
 rm -f application/composer.lock
 composer install -d application --ignore-platform-reqs

ad4690860caae6370cb9e1d5edfd4808fec8763e
Nikita Tarasov <nikita@mygento.ru>
2017-03-22 13:05:54 +0300

    user for php-fpm

--- start.sh
+++ start.sh
@@ -1,7 +1,3 @@
 #!/usr/bin/env sh
 docker-compose up -d
-chmod g+s application
-chmod -R g+w application/public/media
-chmod -R g+w application/public/app/etc
-chmod -R g+w application/public/var
 gulp

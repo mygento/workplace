c428e534fd7a7a9513fb9b1e0e8a25aa53b8090d
Nikita Tarasov <nikita@mygento.ru>
2017-03-22 13:40:19 +0300

    fix ENV variable

--- install_docker.sh
+++ install_docker.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 sudo apt-get update && sudo apt-get install -y apt-transport-https ca-certificates gnupg2 curl
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
-sudo apt-get remove docker docker-engine
+sudo apt-get remove -y docker docker-engine
 sudo echo 'deb [arch=amd64] https://download.docker.com/linux/debian jessie stable' > /etc/apt/sources.list.d/docker.list
 sudo apt-get update && sudo apt-get install -y docker-ce
 sudo groupadd docker
--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env sh
+export USERID=$(id -u)
 docker-compose up -d
 gulp

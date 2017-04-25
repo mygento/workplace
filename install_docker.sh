9d410ceac635441261ae37432c37077df41d10e7
Nikita <luckyraul@users.noreply.github.com>
2017-04-25 10:11:01 +0300

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -10,3 +10,4 @@ sudo service docker restart
 wget "https://github.com/docker/compose/releases/download/1.11.2/docker-compose-$(uname -s)-$(uname -m)" -O docker-compose
 sudo mv docker-compose /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+sudo usermod -a -G www-data ${USER}

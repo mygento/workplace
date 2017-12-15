6fd3ac051870029be130364567ad5ec80858ff9b
Nikita <luckyraul@users.noreply.github.com>
2017-12-15 16:29:22 +0300

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,13 @@ var source = './src';
 var destination = './application';
 
 gulp.task('watch-folder', function() {
-  gulp.src([`${source}/**/*`, `!${source}/node_modules`], {base: source})
+  gulp.src([
+     `${source}/**/*`,
+     `!${source}/node_modules`,
+     `!${source}/vendor`,
+     `!${source}/setup`,
+     `!${source}/dev`
+    ], {base: source})
     .pipe(watch(source, {base: source}))
     .pipe(gulp.dest(destination));
 });

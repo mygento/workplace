9eeaceb94bc9239a57337668967555c170d7d97e
Nikita <luckyraul@users.noreply.github.com>
2018-01-10 17:09:04 +0300

    fix watch

--- gulpfile.js
+++ gulpfile.js
@@ -4,18 +4,20 @@ var debug = require('gulp-debug');
 
 var source = './src';
 var destination = './application';
+var paths = [
+  `${source}/**/*`,
+  `!${source}/.git/**/*`,
+  `!${source}/node_modules/**/*`,
+  `!${source}/vendor/**/*`,
+  `!${source}/setup/**/*`,
+  `!${source}/dev/**/*`,
+  `!${source}/composer.lock`,
+];
 
 gulp.task('watch-folder', function() {
-  gulp.src([
-     `${source}/**/*`,
-     `!${source}/node_modules/**/*`,
-     `!${source}/vendor/**/*`,
-     `!${source}/setup/**/*`,
-     `!${source}/dev/**/*`,
-     `!${source}/composer.lock`,
-    ], { base: source })
+  gulp.src(paths, { base: source})
     .pipe(debug())
-    .pipe(watch(source, { base: source, verbose: true }))
+    .pipe(watch(paths, { base: source, verbose: true }))
     .pipe(gulp.dest(destination));
 });
 

575a78acad35d8a04e625e4d6e1614de76f211c5
m-rybak <30462458+m-rybak@users.noreply.github.com>
2018-04-11 12:29:45 +0300

    Added change logging in gulp (#8)

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,14 @@
 var gulp  = require('gulp');
 var watch = require('gulp-watch');
+var debug = require('gulp-debug');
 
 var source = './application/src';
 var destination = './application/public';
 
 gulp.task('watch-folder', function() {
   gulp.src(source + '/**/*', {base: source})
-    .pipe(watch(source, {base: source}))
+    .pipe(debug())
+    .pipe(watch(source, {base: source, verbose: true}))
     .pipe(gulp.dest(destination));
 });
 

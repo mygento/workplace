9f6b7e8a387dd8850c5c389878e144eacc78408c
nikita tarasov <nikita@mygento.ru>
2019-03-22 18:39:32 +0300

    clean app/code app/design on start

--- start.sh
+++ start.sh
@@ -1,7 +1,16 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 export USERID=$(id -u)
+
+echo -e "\033[37;1;42m \033[5mCleaning app/code \033[0m"
+rm -fR ./application/app/code
+echo
+echo -e "\033[37;1;42m \033[5mCleaning app/design \033[0m"
+echo
+rm -fR ./application/app/design
+
 docker-compose up -d
 if [ -f ./application/bin/magento ]; then
-  docker exec -ti m2-php chmod +x /var/www/public/bin/magento
+  chmod +x ./application/bin/magento
 fi
+
 gulp

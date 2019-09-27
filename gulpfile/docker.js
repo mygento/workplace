7e1f3b5da5d1af8e4aa33d07356f07e4df69b66d
nikita tarasov <nikita@mygento.ru>
2019-09-27 12:27:33 +0300

    docker

--- gulpfile/docker.js
+++ gulpfile/docker.js
@@ -1,7 +1,80 @@
 const spawn = require('child_process').spawn;
+const path = require('path');
+const fs = require('fs');
 
-exports.compose = (cb) => {
-  const cmd = spawn('docker-compose', ['up', '-d'], { stdio: 'inherit' });
+const fileTemplate = (
+  projectRoot,
+  workplaceRoot,
+  projectName,
+  phpImage,
+  nginxImage,
+  nginxPort,
+  dbImage,
+  dbPort
+) => JSON.stringify({
+  version: '3.7',
+  services: {
+    php: {
+      container_name: `${projectName}-php`,
+      image: phpImage,
+      links: ['db'],
+      volumes: [`${projectRoot}:/var/www/public`],
+      environment: ['PHPFPM_USER=$USERID'],
+      depends_on: ['db'],
+    },
+    nginx: {
+      container_name: `${projectName}-nginx`,
+      image: nginxImage,
+      environment: ['NGINX_USER=$USERID'],
+      volumes: [
+        `${workplaceRoot}:/etc/nginx/sites-enabled/`,
+        `${projectRoot}:/var/www/public`,
+      ],
+      ports: [`${nginxPort}:80`],
+      depends_on: ['php'],
+    },
+    db: {
+      container_name: `${projectName}-db`,
+      image: dbImage,
+      command: 'mysqld --character-set-server=utf8 '
+      + ' --collation-server=utf8_general_ci'
+      + ' --innodb_file_per_table',
+      ports: [`${dbPort}:3306`],
+      environment: [
+        'MYSQL_ROOT_PASSWORD=mygento',
+        'MYSQL_DATABASE=magento',
+        'MYSQL_USER=mygento',
+        'MYSQL_PASSWORD=mygento',
+      ],
+      volumes: ['m2-db-data:/var/lib/mysql'],
+    },
+  },
+  volumes: {
+    'm2-db-data': {},
+  },
+}, null, 2);
+
+exports.composeStart = (cb, config) => {
+  process.env.COMPOSE_PROJECT_NAME = config.projectName;
+  process.env.USERID = require('os').userInfo().uid;
+  fs.writeFileSync(
+    'docker-compose.json',
+    fileTemplate(
+      config.appDirectory,
+      path.resolve('../nginx/'),
+      config.projectName,
+      config.php,
+      config.nginx.image,
+      config.nginx.port,
+      config.mysql.image,
+      config.mysql.port
+    )
+  );
+  const cmd = spawn(
+    'docker-compose',
+    ['-f','docker-compose.json', 'up', '-d'],
+    { stdio: 'inherit' }
+  );
   cmd.on('close', function(code) {
     console.log('my-task exited with code ' + code);
     cb(code);
--- gulpfile/index.js
+++ gulpfile/index.js
@@ -5,20 +5,43 @@ const appDirectory = fs.realpathSync(process.env.PWD);
 const resolveApp = relativePath => path.resolve(appDirectory, relativePath);
 const config = require(resolveApp('package.json'));
 
+const workplaceConfig = Object.assign({}, {
+  projectName: 'workplace',
+  php: 'mygento/php:7.2-full',
+  nginx: Object.assign({
+    image: 'luckyraul/nginx:backports',
+    port: 8081,
+  }, config.nginx || {}),
+  mysql: Object.assign({
+    image: 'mygento/mysql',
+    port: 3306,
+  }, config.mysql || {}),
+});
+
+workplaceConfig.appDirectory = appDirectory;
+if (config.name) {
+  workplaceConfig.projectName = config.name;
+}
+
 console.log('gulp');
-console.log(config.workplace);
+console.log('package.json', config.workplace);
+console.log('real', workplaceConfig);
 
 const { watchStyles, watchLive } = require('./watch');
-const { compose } = require('./docker');
+const { composeStart } = require('./docker');
 const style = require('./style');
+
 const watchTask = () => watchStyles(
   config.workplace.theme.map(f => resolveApp(f))
 );
 watchTask.displayName = 'watch';
+
 const liveTask = () => watchLive();
 liveTask.displayName = 'livereload';
-const dockerTask = (cb) => compose(cb);
+
+const dockerTask = (cb) => composeStart(cb, workplaceConfig);
 dockerTask.displayName = 'docker';
+
 const result = Object.assign({
   someMagic: function(cb) {
     cb();

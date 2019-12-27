b8b81722072340b07f7d06821a28758f5ca1623b
nikita tarasov <nikita@mygento.ru>
2019-12-27 17:31:32 +0300

    local config env override

--- gulpfile/docker.js
+++ gulpfile/docker.js
@@ -18,7 +18,7 @@ const getServiceConfig = (config, service) => {
 };
 
 const optionalService = (
-  projectName, name, image, port = false, portDefault, volume = []
+  projectName, name, image, port = false, portDefault, env = [], volume = []
 ) => image ? {
   [name]: Object.assign(
     {},
@@ -32,6 +32,9 @@ const optionalService = (
     } : {},
     volume.length > 0 ? {
       volumes: volume,
+    } : {},
+    env.length > 0 ? {
+      environment: env
     } : {}
   ),
 } : {};
@@ -43,13 +46,13 @@ const fileTemplate = (
   projectRoot,
   workplaceRoot,
   projectName,
-  [phpImage, _phpPort],
-  [nginxImage,  nginxPort],
-  [dbImage, dbPort],
-  [redisImage = null, redisPort = null],
-  [elasticImage = null, elasticPort = null],
-  [varnishImage = null, varnishPort = null],
-  [clickhouseImage = null, clickhousePort = null]
+  [phpImage, _phpPort, phpEnv = []],
+  [nginxImage,  nginxPort, nginxEnv = []],
+  [dbImage, dbPort, dbEnv = []],
+  [redisImage = null, redisPort = null, redisEnv = []],
+  [elasticImage = null, elasticPort = null, elasticEnv = []],
+  [varnishImage = null, varnishPort = null, varnishEnv = []],
+  [clickhouseImage = null, clickhousePort = null, clickhouseEnv = []]
 ) => JSON.stringify({
   version: '3.7',
   services: Object.assign(
@@ -60,14 +63,14 @@ const fileTemplate = (
         image: phpImage,
         networks: [networkName],
         volumes: [`${projectRoot}:/var/www/${projectType}`],
-        environment: ['PHPFPM_USER=$USERID'],
+        environment: ['PHPFPM_USER=$USERID', ...phpEnv],
         depends_on: ['db'],
       },
       nginx: {
         container_name: `${projectName}-nginx`,
         image: nginxImage,
         networks: [networkName],
-        environment: ['NGINX_USER=$USERID'],
+        environment: ['NGINX_USER=$USERID', ...nginxEnv],
         volumes: [
           `${workplaceRoot}:/etc/nginx/sites-enabled/`,
           `${projectRoot}:/var/www/${projectType}`,
@@ -88,14 +91,15 @@ const fileTemplate = (
           `MYSQL_DATABASE=${projectType}`,
           'MYSQL_USER=mygento',
           'MYSQL_PASSWORD=mygento',
+          ...dbEnv
         ],
         volumes: [`${getVolumeName(projectType, 'db')}:/var/lib/mysql`],
       },
     },
-    optionalService(projectName, 'redis', redisImage, redisPort, 6379, []),
-    optionalService(projectName, 'elastic', elasticImage, elasticPort, 9200, ['elastic:/usr/share/elasticsearch/data']),
-    optionalService(projectName, 'varnish', varnishImage, varnishPort, 8081, []),
-    optionalService(projectName, 'clickhouse', clickhouseImage, clickhousePort, 8123, ['clickhouse:/var/lib/clickhouse'])
+    optionalService(projectName, 'redis', redisImage, redisPort, 6379, redisEnv, []),
+    optionalService(projectName, 'elastic', elasticImage, elasticPort, 9200, elasticEnv, ['elastic:/usr/share/elasticsearch/data']),
+    optionalService(projectName, 'varnish', varnishImage, varnishPort, 8081, varnishEnv, []),
+    optionalService(projectName, 'clickhouse', clickhouseImage, clickhousePort, 8123, clickhouseEnv, ['clickhouse:/var/lib/clickhouse'])
   ),
   volumes: Object.assign(
     {},
@@ -168,9 +172,9 @@ exports.composeCommand = (cb, command, config) => {
       config.appDirectory,
       path.resolve(`../nginx/${config.type}`),
       config.projectName,
-      [config.php.image, config.php.port],
-      [config.nginx.image,  config.nginx.port],
-      [config.mysql.image,  config.mysql.port],
+      [config.php.image, config.php.port, config.php.env],
+      [config.nginx.image,  config.nginx.port, config.nginx.env],
+      [config.mysql.image,  config.mysql.port, config.mysql.env],
       getServiceConfig(config, 'redis'),
       getServiceConfig(config, 'elasticsearch'),
       getServiceConfig(config, 'varnish'),
